527c8cb5f164491d6daa1c46f309ff22fc42d151
aguerremo <aguerremo@gmail.com>
2025-04-13 15:54:11 +0200

    4.19 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
   try{
@@ -11,22 +12,50 @@ blogsRouter.get('/', async (request, response, next) => {
   }
  })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  try{
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next (error)
+  }     
 })
+
+const getTokenFrom = request => {
+  console.log('Funciona 1')
+  const authorization = request.get('authorization')
+  console.log('Funciona 2', authorization)
+    if(authorization && authorization.startsWith('Bearer ')){
+      console.log('Funciona 3')
+      return authorization.replace('Bearer ', '') 
+    }
+    console.log('Funciona 3.1')
+    return null
+  
+}
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  console.log('Funciona 4')
 
-  const user = await User.findById(body.user)
+  const token = getTokenFrom(request)
+
+  let decodedToken
+
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  } catch {}
+  
+  if (!token || !decodedToken.id) {
+
+    return response.status(401).json({ error: 'token invalid'})
+  } else {
+
+    const user = await User.findById(decodedToken.id)
   
     const blog = new Blog({
       title: body.title,
@@ -38,15 +67,17 @@ blogsRouter.post('/', async (request, response, next) => {
 
     try {
       const savedBlog = await blog.save()
-    
-      await user.save()
       user.blogs = user.blogs.concat(savedBlog.id)
+      await user.save()
       response.json(savedBlog)
 
     } catch (error) {
         next(error)
     }
 
+
+  }
+  
 })
 
 
--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const loginRouter = require('express').Router()
 const User = require('../models/User')
 
@@ -12,15 +13,23 @@ loginRouter.post('/', async (request, response) => {
   ? false
   : await bcrypt.compare(password, user.passwordHash)
 
-  if (!passwordCorrect) {
+  if (!(user && passwordCorrect)) {
     response.status(401).json({
       error: 'invalid user or password'
     })
   }
 
+  const userForToken = {
+    id: user._id, //Probr con .id
+    username: user.username
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET) 
+
   response.send({
     name: user.name,
-    username: user.username
+    username: user.username,
+    token
   })
 })
 
