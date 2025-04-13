3755a061655cb8612f6252b1a1b49c1c4f1b3171
aguerremo <aguerremo@gmail.com>
2025-04-13 13:09:34 +0200

    4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,8 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
-
-
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -27,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 
--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -3,5 +3,25 @@ const loginRouter = require('express').Router()
 const User = require('../models/User')
 
 loginRouter.post('/', async (request, response) => {
-  const body = request.body
+  const { body } = request
+  const { username, password} = body
+
+  const user = await User.findOne({ username })
+
+  const passwordCorrect = user === null
+  ? false
+  : await bcrypt.compare(password, user.passwordHash)
+
+  if (!passwordCorrect) {
+    response.status(401).json({
+      error: 'invalid user or password'
+    })
+  }
+
+  response.send({
+    name: user.name,
+    username: user.username
+  })
 })
+
+module.exports = loginRouter
