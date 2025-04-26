c3440d31348312da6bc20ee3e10a0959141768eb
aguerremo <aguerremo@gmail.com>
2025-04-26 21:49:03 +0200

    6.1 and 6.2 done

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,59 +1,41 @@
 import {createStore} from 'redux'
 
-initialState = {
+const initialState = {
   good: 0,
   ok: 0,
   bad: 0
-}
+} 
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return initialState.good + 1
+      return {...state, good: state.good + 1}
     case 'OK':
-      return initialState.ok + 1
+      return {...state, ok: state.ok + 1}
     case 'BAD':
-      return initialState.bad - 1
+      return {...state, bad: state.bad - 1}
     case 'ZERO':
-      return initialState = 0
+        return {good: 0, ok: 0, bad: 0}
     default: return state
+
+      }
+    
   }
-  
-}
 
 const store = createStore(counterReducer)
 
 store.subscribe(() => {
   const storeNow = store.getState()
   console.log('storeNow: ', storeNow)
+
 })
 
 
 store.dispatch({type: 'GOOD'})
 store.dispatch({type: 'GOOD'})
-store.dispatch({type: 'GOOD'})
+store.dispatch({type: 'OK'})
 store.dispatch({ type: 'ZERO' })
 store.dispatch({ type: 'BAD' })
-
-
-const actionGood = {
-  type: 'GOOD'
-}
-
-const actionOk = {
-  type: 'OK'
-}
-
-const actionBad = {
-  type: 'BAD'
-}
-
-const actionZero = {
-  type: 'ZERO'
-}
-
-
-
 
 export default counterReducer
